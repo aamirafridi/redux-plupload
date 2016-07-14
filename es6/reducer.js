ff08cdf2f377a3407660c776712c3e7a54c21a27
Rouan Wilsenach <Rouan.Wilsenach@tesglobal.com>
2016-07-14 13:12:33 +0200

    add createReducer function which can be used by a consumer to create a reducer for a specific uploader's actions

--- es6/reducer.js
+++ es6/reducer.js
@@ -1,26 +1,30 @@
 import { ActionTypes, DEFAULT_UPLOADER_HANDLE } from './constants';
 
-export default function reducer(state = {}, { type, meta: { uploader } = {} }) {
-  switch (type) {
-    case ActionTypes.INITING:
-    case ActionTypes.POST_INIT:
-    case ActionTypes.OPTION_CHANGED:
-    case ActionTypes.REFRESHING:
-    case ActionTypes.STATE_CHANGED:
-    case ActionTypes.UPLOAD_FILE:
-    case ActionTypes.BEFORE_UPLOAD:
-    case ActionTypes.QUEUE_CHANGED:
-    case ActionTypes.UPLOAD_PROGRESS:
-    case ActionTypes.FILES_REMOVED:
-    case ActionTypes.FILE_FILTERED:
-    case ActionTypes.FILES_ADDED:
-    case ActionTypes.FILE_UPLOADED:
-    case ActionTypes.CHUNK_UPLOADED:
-    case ActionTypes.UPLOAD_COMPLETE:
-    case ActionTypes.ERROR:
-    case ActionTypes.DESTROYING:
-      return uploader.handle === DEFAULT_UPLOADER_HANDLE ? uploader : state;
-    default:
-      return state;
-  }
+export function createReducer(uploaderHandle = DEFAULT_UPLOADER_HANDLE) {
+  return function reducer(state = {}, { type, meta: { uploader } = {} }) {
+    switch (type) {
+      case ActionTypes.INITING:
+      case ActionTypes.POST_INIT:
+      case ActionTypes.OPTION_CHANGED:
+      case ActionTypes.REFRESHING:
+      case ActionTypes.STATE_CHANGED:
+      case ActionTypes.UPLOAD_FILE:
+      case ActionTypes.BEFORE_UPLOAD:
+      case ActionTypes.QUEUE_CHANGED:
+      case ActionTypes.UPLOAD_PROGRESS:
+      case ActionTypes.FILES_REMOVED:
+      case ActionTypes.FILE_FILTERED:
+      case ActionTypes.FILES_ADDED:
+      case ActionTypes.FILE_UPLOADED:
+      case ActionTypes.CHUNK_UPLOADED:
+      case ActionTypes.UPLOAD_COMPLETE:
+      case ActionTypes.ERROR:
+      case ActionTypes.DESTROYING:
+        return uploader.handle === uploaderHandle ? uploader : state;
+      default:
+        return state;
+    }
+  };
 }
+
+export default createReducer();
--- test/reducer.js
+++ test/reducer.js
@@ -1,6 +1,6 @@
 import expect from 'expect.js';
 
-import { ActionTypes, reducer } from '../es6/index';
+import { ActionTypes, reducer, createReducer } from '../es6/index';
 
 describe('reducer', () => {
   it('returns empty state when passed undefined and non-uploader action', () => {
@@ -22,6 +22,22 @@ describe('reducer', () => {
     const oldState = {};
     const newState = reducer(oldState, { type: 'FOO', meta: { uploader } });
     expect(newState).to.not.be(uploader);
+    expect(newState).to.be(oldState);
+  });
+});
+
+describe('createReducer', () => {
+  it('creates a reducer which updates state on its uploader actions', () => {
+    const myReducer = createReducer('my-uploader');
+    const uploader = { handle: 'my-uploader' };
+    const newState = myReducer(undefined, { type: ActionTypes.INITING, meta: { uploader } });
+    expect(newState).to.be(uploader);
+  });
+  it('creates a reducer which returns old state on actions from other uploaders', () => {
+    const myReducer = createReducer('my-uploader');
+    const uploader = { handle: 'not-my-uploader' };
+    const oldState = {};
+    const newState = myReducer(oldState, { type: ActionTypes.INITING, meta: { uploader } });
     expect(newState).to.be(oldState);
   });
 });
